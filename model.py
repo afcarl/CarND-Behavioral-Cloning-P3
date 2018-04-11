154e22259167752babf5bc867e8e4a4d6a95eee1
MJK <mjk@emmjaykay.org>
2018-04-10 23:25:43 -0700

    nVidia driving model

--- model.py
+++ model.py
@@ -26,12 +26,20 @@ def grayscale(img):
 
 def blur(img):
     return cv2.blur(img, (6,6))
-    
+
+def crop_img(img):
+    start_of_landscape = 60
+    length_of_windshield = 70
+    cropped_y = start_of_landscape + length_of_windshield
+    cropped_img = img[start_of_landscape:cropped_y, 0:320]
+    return cropped_img
+
 def preprocess(img):
     # this is our main preprocess pipeline
 
-    img1 = grayscale(img)
-    img1.reshape((160,320,1))
+    # img1 = grayscale(img)
+    # img1.reshape((160,320,1))
+    img1 = img
     # grayscale has to come first for cv2
     # error: (-215) depth == CV_8U || depth == CV_16U || depth == CV_32F
     # in function cvtColor
@@ -41,6 +49,8 @@ def preprocess(img):
     # TODO: Cut image sizes, add filter polygons
     # img3 = filter_images(img2)
     img3 = blur(img2)
+
+    # img4 = crop_img(img3)
 
     return img3
 
@@ -86,30 +96,28 @@ def process_pipeline(img_data):
     #     data['X']['left_image'] = preprocess(data['X']['left_image'])
     #     data['X']['right_image'] = preprocess(data['X']['right_image'])
 
-    for i, img in enumerate(img_data):
-        img_data[i] = preprocess(img)
-    return img_data
+    return preprocess(img_data)
 
-def model_basic():
+def model_basic(input_shape=None):
 # define models
 
     from keras.models import Sequential
     from keras.layers import Flatten, Dense, Lambda
 
     model = Sequential()
-    model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+    model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(70,320,3)))
     model.add(Flatten())
     model.add(Dense(1))
 
     return model
 
-def model_lenet():
+def model_lenet(input_shape=None):
     from keras.models import Sequential
     from keras.layers import Flatten, Dense, Lambda
     from keras.layers.convolutional import Convolution2D, MaxPooling2D
 
     model = Sequential()
-    model.add(Convolution2D(6,5,5, activation='relu', input_shape=(160,320,3)))
+    model.add(Convolution2D(6,5,5, activation='relu', input_shape=input_shape))
     model.add(MaxPooling2D())
     model.add(Convolution2D(6,5,5, activation='relu'))
     model.add(MaxPooling2D())
@@ -120,6 +128,83 @@ def model_lenet():
 
     return model
 
+def model_nvidia(input_shape=None):
+    from keras.models import Sequential
+    from keras.layers import Flatten, Dense, Convolution2D, MaxPooling2D, Dropout, BatchNormalization, Activation
+
+    # activation = "relu"
+    # model = Sequential()
+    #
+    # # Normalize
+    # # model.add(BatchNormalization(input_shape=input_shape, axis=1))
+    #
+    # model.add(Convolution2D(24, 5, 5, activation=activation, input_shape=input_shape, name="convolution0"))
+    # model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+    #
+    # model.add(Convolution2D(36, 5, 5,  activation=activation, name="convolution1"))
+    # model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+    #
+    # model.add(Convolution2D(48, 5, 5, activation=activation, name="convolution2"))
+    # model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+    #
+    # model.add(Convolution2D(64, 3, 3, activation=activation, name="convolution3"))
+    # model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+    #
+    # model.add(Convolution2D(64, 3, 3, activation=activation, name="convolution4"))
+    # model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+    #
+    #
+    # model.add(Flatten())
+    # # model.add(Dropout(0.2))
+    # # model.add(Activation(activation=activation))
+    #
+    # model.add(Dense(1164, activation=activation, name="dense1"))
+    # # model.add(Dropout(0.5))
+    # # model.add(Activation(activation=activation))
+    #
+    # model.add(Dense(100, activation=activation, name="dense2"))
+    # # model.add(Dropout(0.5))
+    # # model.add(Activation(activation=activation))
+    #
+    # model.add(Dense(50, activation=activation, name="dense3"))
+    # # model.add(Dropout(0.5))
+    # # model.add(Activation(activation=activation))
+    #
+    # model.add(Dense(10, activation=activation, name="dense4"))
+    # # model.add(Dropout(0.5))
+    # # model.add(Activation(activation=activation))
+    # model.add(Dense(1, name="output"))
+
+    activation = 'relu'
+
+    model = Sequential()
+    model.add(Convolution2D(24, 5, 5, activation=activation, input_shape=input_shape))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(36, 5, 5, activation=activation))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(48, 5, 5, activation=activation))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(64, 3,3, activation=activation))
+    model.add(MaxPooling2D())
+    # model.add(Convolution2D(64, 1, 1, activation=activation))
+    # model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(1164))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(100))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(50))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(10))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(1))
+    return model
+
+
 def _retrieve_center_image(data):
     return data['X']['center_image']
 
@@ -129,6 +214,8 @@ def retrieve_images_and_labels(img_data):
     for img in img_data:
         center_img_data = _retrieve_center_image(data=img)
         measurement = _retrieve_steering_angle(data=img)
+
+        center_img_data = process_pipeline(center_img_data)
 
         images.append(center_img_data)
         measurements.append(measurement)
@@ -156,9 +243,9 @@ images, measurements = retrieve_images_and_labels(img_data)
 X = np.array(images)
 y = np.array(measurements)
 # model = model_basic()
-model = model_lenet()
+model = model_nvidia(input_shape=(160,320,3))
 model.compile(loss='mse', optimizer='adam')
-model.fit(X, y, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X, y, validation_split=0.2, shuffle=True, nb_epoch=2)
 
 model.save('model.h5')
 
