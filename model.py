907e85c52a738847346ff7d65eb789d2dbf76f80
MJK <mjk@emmjaykay.org>
2018-04-11 23:06:57 -0700

    lambda and cropping

--- model.py
+++ model.py
@@ -250,6 +250,8 @@ def model_nvidia(input_shape=None):
     activation = 'relu'
 
     model = Sequential()
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((50,20), (0,0))))
     model.add(Convolution2D(24, 5, 5, activation=activation, input_shape=input_shape))
     model.add(MaxPooling2D())
     model.add(Convolution2D(36, 5, 5, activation=activation))
